e6b1f2207066e50f41055c4d59a66672f28c8ee2
Donny Xie <donnior@gmail.com>
2013-08-31 14:49:56 +0800

    refactor

--- src/main/java/me/donnior/sparkle/internal/ActionMethodDefinitionFinder.java
+++ src/main/java/me/donnior/sparkle/internal/ActionMethodDefinitionFinder.java
@@ -16,6 +16,60 @@ import com.google.common.collect.Lists;
 
 public class ActionMethodDefinitionFinder {
 
+    static class DefaultActionMethodDefinition implements ActionMethodDefinition{
+
+        private final Method method;
+        private final String actionName;
+        private final List<ActionParamDefinition> apds;
+
+        public DefaultActionMethodDefinition(Method method, String actionName){
+            this.method = method;
+            this.actionName = actionName;
+            
+            final Class<?>[] paramTypes = method.getParameterTypes();
+            final Annotation[][] ans = method.getParameterAnnotations();
+          
+            List<ActionParamDefinition> apds = Lists.newArrayList();
+            for(int i=0; i<paramTypes.length; i++){
+                final Class<?> type = paramTypes[i];
+                final Annotation[] annotaions = ans[i];
+                ActionParamDefinition apd = new DefaulActionParamDefinition(type, Arrays.asList(annotaions));
+                apds.add(apd);
+            }
+            
+            this.apds = apds;
+            
+        }
+        
+        @Override
+        public String actionName() {
+            return this.actionName;
+        }
+
+        @Override
+        public Method method() {
+            return this.method;
+        }
+
+        @Override
+        public List<ActionParamDefinition> paramDefinitions() {
+            return this.apds;
+        }
+
+        @Override
+        public List<Annotation> annotions() {
+            return Arrays.asList(this.method().getAnnotations());
+        }
+
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        @Override
+        public boolean hasAnnotation(Class annotationType) {
+            return this.method().isAnnotationPresent(annotationType);
+        }
+        
+    }
+    
+    //TODO add cache here
     public ActionMethodDefinition find(Class<? extends Object> clz, final String actionName) {
         
         Set<Method> methods = ReflectionUtils.getAllMethods(clz, Predicates.and(ReflectionUtils.withModifier(Modifier.PUBLIC),ReflectionUtils.withName(actionName)));
@@ -25,47 +79,8 @@ public class ActionMethodDefinitionFinder {
         if(methods.size() > 1){
             throw new SparkleException("find more than actions with same name " + actionName);
         }
-        final Method method = methods.iterator().next();        
-        return new ActionMethodDefinition() {
-            
-            @Override
-            public List<ActionParamDefinition> paramDefinitions() {
-                final Class<?>[] paramTypes = method.getParameterTypes();
-                final Annotation[][] ans = method.getParameterAnnotations();
-              
-                List<ActionParamDefinition> apds = Lists.newArrayList();
-                for(int i=0; i<paramTypes.length; i++){
-                    final Class<?> type = paramTypes[i];
-                    final Annotation[] annotaions = ans[i];
-                    ActionParamDefinition apd = new DefaulActionParamDefinition(type, Arrays.asList(annotaions));
-                    apds.add(apd);
-                }
-                
-                return apds;
-            }
-            
-            @Override
-            public Method method() {
-                return method;
-            }
-            
-            @SuppressWarnings({ "rawtypes", "unchecked" })
-            @Override
-            public boolean hasAnnotation(Class annotationType) {
-                return this.method().isAnnotationPresent(annotationType);
-            }
-            
-            @Override
-            public List<Annotation> annotions() {
-                return Arrays.asList(this.method().getAnnotations());
-            }
-            
-            @Override
-            public String actionName() {
-                return actionName;
-            }
-        };
-        
+        final Method method = methods.iterator().next();
+        return new DefaultActionMethodDefinition(method, actionName);
     }
 
 }
