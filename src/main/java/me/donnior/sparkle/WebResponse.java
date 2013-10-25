43ea9829b3a0fd332eecae9f19b0ba9e7e77696e
Donny Xie <donnior@gmail.com>
2013-10-25 16:14:45 +0800

    //TODO, should WebResponse be introduced?

---
+++ src/main/java/me/donnior/sparkle/WebResponse.java
@@ -0,0 +1,10 @@
+package me.donnior.sparkle;
+
+import javax.servlet.http.HttpServletResponse;
+
+//TODO should be introduced?
+public interface WebResponse {
+    
+    HttpServletResponse getServletResponse();
+    
+}
